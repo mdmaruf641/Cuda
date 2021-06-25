f38d386ce42b333c6aef5bb9a89dea1ea446d31f
Md Maruf <83667585+mdmaruf641@users.noreply.github.com>
2021-06-24 21:38:12 -1200

    Add files via upload

---
+++ resources/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    
+    //sticky menu
+    
+    $(".js--services-section").waypoint(function(direction){
+        if(direction =="down"){
+            $("nav").addClass("sticky");
+        }
+        
+        else {
+            $("nav").removeClass("sticky")
+        }
+    });
+    
+    
+    //mixitup (portfolio)
+    var mixer = mixitup('.container');
+});
